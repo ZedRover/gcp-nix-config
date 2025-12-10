2d640a5f5884bf74339dfb7795acf0ee0acbbd79
yu.wang <yu.wang@outlook.my>
2025-12-10 12:37:00 +0000

    update

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 nix run --impure home-manager/master -- switch --flake . -b backup
-echo /home/zed/.nix-profile/bin/zsh | sudo tee -a /etc/shells
-chsh -s /home/zed/.nix-profile/bin/zsh
+echo "$HOME/.nix-profile/bin/zsh" | sudo tee -a /etc/shells
+chsh -s "$HOME/.nix-profile/bin/zsh"
--- flake.nix
+++ flake.nix
@@ -11,8 +11,8 @@
     let
       # 1. 这里直接硬编码你的系统信息，最稳
       system = "x86_64-linux";
-      username = "zed"; 
-      homeDirectory = "/home/zed";
+      username = "ubuntu"; 
+      homeDirectory = "/home/ubuntu";
 
       pkgs = nixpkgs.legacyPackages.${system};
       
@@ -29,10 +29,10 @@
       };
     in {
       homeConfigurations = {
-        # 2. 同时暴露 "zed" 和 "zed@ml-training" 两个入口
+        # 2. 同时暴露 "ubuntu" 和 "ubuntu@ml-training" 两个入口
         # 这样无论 home-manager 怎么猜都能猜中
-        "zed" = myHomeConfig;
-        "zed@ml-training" = myHomeConfig;
+        "ubuntu" = myHomeConfig;
+        "ubuntu@ml-training" = myHomeConfig;
       };
     };
 }
